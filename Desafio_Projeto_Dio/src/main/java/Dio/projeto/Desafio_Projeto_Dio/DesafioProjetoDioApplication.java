74eeb99d5e9bcc1f3c5338029e63da18f7badfc5
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-12 19:23:05 -0300

    firstcommit

---
+++ Desafio_Projeto_Dio/src/main/java/Dio/projeto/Desafio_Projeto_Dio/DesafioProjetoDioApplication.java
@@ -0,0 +1,13 @@
+package Dio.projeto.Desafio_Projeto_Dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProjetoDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProjetoDioApplication.class, args);
+	}
+
+}
---
+++ Desafio_Projeto_Dio/src/test/java/Dio/projeto/Desafio_Projeto_Dio/DesafioProjetoDioApplicationTests.java
@@ -0,0 +1,13 @@
+package Dio.projeto.Desafio_Projeto_Dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProjetoDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
